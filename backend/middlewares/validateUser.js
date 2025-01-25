985d267d10a270fd2756d306b1a2b3e2c430d4fa
aleksysto <a.stocki.060@studms.ug.edu.pl>
2025-01-25 21:34:59 +0100

    Added validation functionalities to user models and endpoints

---
+++ backend/middlewares/validateUser.js
@@ -0,0 +1,28 @@
+const { User } = require("../models/userModel");
+const { checkPassword } = require("../utils/validation");
+
+const validateUserData = async (req, res, next) => {
+    try {
+        const userData = { ...req.body };
+        if (!checkPassword(userData.password)) {
+            throw new Error("ValidationError")
+        }
+
+        const user = new User(userData);
+
+        await user.validate();
+
+        next();
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            const errors = Object.values(error.errors).map((err) => err.message);
+            return res
+                .status(400)
+                .json({ message: "Validation error", details: errors });
+        }
+
+        next(error);
+    }
+};
+
+module.exports = validateUserData;
