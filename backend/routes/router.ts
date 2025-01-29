992d03758abec9ae6453f2a6a785908ac05a725e
aleksysto <a.stocki.060@studms.ug.edu.pl>
2025-01-29 22:38:27 +0100

    Added image upload routes, instructions for their use

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -3,10 +3,12 @@ import projects from './project/projectRoutes'
 import authRoutes from './auth/authRoutes'
 import tokenDecode from './token/tokenDecode'
 import userRoutes from "./user/userRoutes"
+import imageRoutes from './image/imageRoutes'
 const router = express.Router();
 
 router.use(tokenDecode);
 router.use(projects);
 router.use(authRoutes);
 router.use(userRoutes);
+router.use(imageRoutes);
 export default router
